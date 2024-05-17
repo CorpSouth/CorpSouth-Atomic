d8b7fb099a142e4e64bd30a2d033341ea462846c
corpsouth <nixontech91@hotmail.com>
2024-05-17 09:08:23 -0500

    Clean up nautilus-open-any-terminal.sh, make sure it only targets nautilus

--- config/scripts/nautilus-open-any-terminal.sh
+++ config/scripts/nautilus-open-any-terminal.sh
@@ -1,21 +1,16 @@
 #!/usr/bin/env bash
 
-# Tell this script to exit if there are any errors.
-# You should have this in every custom script, to ensure that your completed
-# builds actually ran successfully without any errors!
 set -oue pipefail
-
-# Your code goes here.
 
 echo 'Installing make...'
 rpm-ostree install make
 echo 'Cloning nautilus-open-any-terminal source repository...'
 cd /tmp
 git clone https://github.com/KyleGospo/nautilus-open-any-terminal
-cd nautilus-open-any-terminal/
 echo 'Entering repository and running make...'
+cd nautilus-open-any-terminal/
 make
-make install schema
+make install-nautilus schema
 echo 'Exiting repo and cleaning up...'
 cd
 rm -rf /tmp/nautilus-open-any-terminal
