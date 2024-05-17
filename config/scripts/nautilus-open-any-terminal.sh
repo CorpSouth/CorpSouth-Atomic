64023beb95e3a005c0cc9f2dbbb392c6afd492c0
corpsouth <nixontech91@hotmail.com>
2024-05-17 08:53:05 -0500

    Opting to build nautilus-open-any-terminal instead of relying on copr

--- config/scripts/nautilus-open-any-terminal.sh
+++ config/scripts/nautilus-open-any-terminal.sh
@@ -12,9 +12,11 @@ rpm-ostree install make
 echo 'Cloning nautilus-open-any-terminal source repository...'
 cd /tmp
 git clone https://github.com/KyleGospo/nautilus-open-any-terminal
+cd nautilus-open-any-terminal/
 echo 'Entering repository and running make...'
 make
 make install schema
 echo 'Exiting repo and cleaning up...'
 cd
 rm -rf /tmp/nautilus-open-any-terminal
+rpm-ostree uninstall make
