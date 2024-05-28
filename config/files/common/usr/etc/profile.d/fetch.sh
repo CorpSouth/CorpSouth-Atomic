3f95b7ddefb46e123fecf0800a18f6250706dd04
CorpSouth <nixontech91@hotmail.com>
2024-05-28 18:24:57 -0500

    Removing custom extensions and shell configurations to keep things clean and future-stable

--- config/files/common/usr/etc/profile.d/fetch.sh
+++
@@ -1,7 +0,0 @@
-if [ -x "/usr/bin/fastfetch" ]; then
-	fastfetch
-     elif [ -x "/usr/bin/neofetch" ]; then
-	neofetch
-     else
-        uname -a
-fi;
--- config/files/common/usr/etc/profile.d/liquidprompt-interactive-only.sh
+++
@@ -1,2 +0,0 @@
-# Only load Liquid Prompt in interactive shells, not from a script or from scp
-[[ $- = *i* ]] && source /usr/etc/liquidprompt/liquidprompt
